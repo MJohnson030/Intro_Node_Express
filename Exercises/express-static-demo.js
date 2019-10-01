67d5f06aaa1ac8cf22dc2527a04b7eb176c93933
Ryan <rburkhart014@west-mec.org>
2019-10-01 15:20:09 -0700

    Ended off @ the beginning of step 11

---
+++ Exercises/express-static-demo.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var express = require('express');
+var app = express();
+
+var app = express();
+    var port = 8080;
+
+    app.listen(port, function() {
+        console.log('Server listening on localhost:%s', port);
+    });
+
+    var port = 8080;
+   app.use(express.static(__dirname));
